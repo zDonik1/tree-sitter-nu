72c0b2c9fda26417e09b6cc6e61cce74d4beaf6c
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2024-03-07 04:29:31 +0500

    Add treesitter indent rules

---
+++ queries/nu/indents.scm
@@ -0,0 +1,19 @@
+[
+  (val_record)
+  (val_list)
+  (val_closure)
+  (val_table)
+  (block)
+] @indent.begin
+
+[
+  "}"
+  "]"
+] @indent.end
+
+[
+  "}"
+  "]"
+] @indent.branch
+
+(comment) @indent.auto
